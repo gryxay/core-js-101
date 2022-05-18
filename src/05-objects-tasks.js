fe018bd70e989f57603b8884ab32e46e1a70b816
gryxay <72890533+gryxay@users.noreply.github.com>
2022-05-18 23:31:09 +0300

    feat: add some object solutions

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,14 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() {
+      return this.width * this.height;
+    },
+  };
 }
 
 
@@ -35,8 +41,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +57,8 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.setPrototypeOf(JSON.parse(json), proto);
 }
 
 
@@ -109,6 +115,65 @@ function fromJSON(/* proto, json */) {
  *
  *  For more examples see unit tests.
  */
+/*
+const cssSelectorBuilder = {
+  element(value) {
+    return {
+      stringify() {
+        return value;
+      },
+    };
+  },
+
+  id(value) {
+    return {
+      stringify() {
+        return `#${value}`;
+      },
+    };
+  },
+
+  class(value) {
+    return {
+      stringify() {
+        return `.${value}`;
+      },
+    };
+  },
+
+  attr(value) {
+    return {
+      stringify() {
+        return `[${value}]`;
+      },
+    };
+  },
+
+  pseudoClass(value) {
+    return {
+      stringify() {
+        return `:${value}`;
+      },
+    };
+  },
+
+  pseudoElement(value) {
+    return {
+      stringify() {
+        return `::${value}`;
+      },
+    };
+  },
+
+  combine(selector1, combinator, selector2) {
+    return {
+      stringify() {
+        return `${selector1} ${combinator} ${selector2}`;
+      },
+    };
+  },
+};
+*/
 
 const cssSelectorBuilder = {
   element(/* value */) {
